af746a4e7449657de8e247a0869a533beab7fc5b
buscot7 <buscot7@gmail.com>
2017-11-05 18:35:12 +0100

    create reviews routes

---
+++ src/WCS/CoavBundle/Form/ReviewType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WCS\CoavBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReviewType extends AbstractType {
+
+}
